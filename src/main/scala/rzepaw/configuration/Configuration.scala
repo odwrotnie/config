642f4ebeca7e583e7425d4e4afd922f7930cd248
rzepaw <odwrotnie@gmail.com>
2017-03-12 20:16:38 +0100

    cfg

--- src/main/scala/rzepaw/configuration/Configuration.scala
+++ src/main/scala/rzepaw/configuration/Configuration.scala
@@ -3,22 +3,25 @@ package rzepaw.configuration
 import com.typesafe.config._
 import com.typesafe.scalalogging.LazyLogging
 
+import scala.util.Try
+
 trait Configuration
   extends LazyLogging {
 
   lazy val configuration: Config = {
 
     val MODE_NAME = "configuration.mode"
-    val DEFAULT_CONF = "default"
 
     val rootConfig = ConfigFactory.load
 
     val mode: String = rootConfig.getString(MODE_NAME)
     logger.debug(s"Using $mode config")
 
-    val modeConfig = rootConfig.getConfig(mode)
-    val defaultConfig = rootConfig.getConfig(DEFAULT_CONF)
+    val modeConfig: Option[Config] = Try(rootConfig.getConfig(mode)).toOption
 
-    modeConfig.withFallback(defaultConfig)
+    modeConfig match {
+      case Some(cfg) => cfg.withFallback(rootConfig)
+      case None => rootConfig
+    }
   }
 }
